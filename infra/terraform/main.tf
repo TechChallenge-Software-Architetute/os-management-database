cec34b20739f2d11d95f066294fb6221a9dc008a
Giovanni <gmontezano79@gmail.com>
2026-08-25 22:32:20 -0300

    sg name

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -3,7 +3,7 @@
 ############################################
 
 resource "aws_db_subnet_group" "aurora_subnets" {
-  name       = "aurora-workshop-subnet-group-3"
+  name       = "aurora-workshop-subnet-group"
   subnet_ids = var.subnet_ids
 
   tags = {
@@ -16,7 +16,7 @@ resource "aws_db_subnet_group" "aurora_subnets" {
 ############################################
 
 resource "aws_security_group" "aurora_sg" {
-  name        = "aurora-security-group-3"
+  name        = "aurora-security-group"
   description = "Acesso ao Aurora MySQL"
   vpc_id      = var.vpc_id
 
