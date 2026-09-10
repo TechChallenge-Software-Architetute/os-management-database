5eb5c7eb474631a978bf3909ae25f81fe55ca0ff
Giovanni <gmontezano79@gmail.com>
2026-09-10 17:18:13 -0300

    ajuste script

--- scripts/dml.sql
+++ scripts/dml.sql
@@ -78,8 +78,9 @@ INSERT INTO service_type (id, name, description) VALUES
 ON CONFLICT (name) DO NOTHING;
 
 -- service (status as jsonb)
-INSERT INTO service (service_type_name, id_os, service_status)
+INSERT INTO service (id, service_type_name, id_os, service_status)
 VALUES (
+  gen_random_uuid(),
   'TROCA_OLEO',
   'a46ac51b-5ca6-439b-ba52-a36bd52e8647',
   '[{"status":"TO_DO","changedAt":"2026-04-30T14:35:00"}]'::jsonb
