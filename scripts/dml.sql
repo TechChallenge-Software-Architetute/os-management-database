0c254f70eef897bb79f44fd547d21605fe0efe80
Giovanni <gmontezano79@gmail.com>
2026-09-10 17:36:08 -0300

    ajuste

--- scripts/dml.sql
+++ scripts/dml.sql
@@ -1,10 +1,23 @@
 -- DML: inserts e seeds idempotentes
 
 -- roles + admin user
-INSERT INTO roles (id, name) VALUES
-  (uuid_generate_v4(), 'ROLE_ADMIN'),
-  (uuid_generate_v4(), 'ROLE_USER'),
-  (uuid_generate_v4(), 'ROLE_TECHNICIAN');
+INSERT INTO roles (id, name)
+SELECT uuid_generate_v4(), 'ROLE_ADMIN'
+WHERE NOT EXISTS (
+  SELECT 1 FROM roles WHERE name = 'ROLE_ADMIN'
+);
+
+INSERT INTO roles (id, name)
+SELECT uuid_generate_v4(), 'ROLE_USER'
+WHERE NOT EXISTS (
+  SELECT 1 FROM roles WHERE name = 'ROLE_USER'
+);
+
+INSERT INTO roles (id, name)
+SELECT uuid_generate_v4(), 'ROLE_TECHNICIAN'
+WHERE NOT EXISTS (
+  SELECT 1 FROM roles WHERE name = 'ROLE_TECHNICIAN'
+);
 
 -- superadmin user (bcrypt hash from seu script)
 INSERT INTO users (id, email, password)
