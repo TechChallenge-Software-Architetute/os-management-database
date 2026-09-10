2fab0258ebe56c1004ee5f8fa5a92c3f7a44cfb1
Giovanni <gmontezano79@gmail.com>
2026-09-10 14:30:09 -0300

    disable visibility

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -56,7 +56,7 @@ resource "aws_db_instance" "postgres" {
   db_subnet_group_name   = aws_db_subnet_group.aurora_subnets.name
   vpc_security_group_ids = [aws_security_group.aurora_sg.id]
 
-  publicly_accessible     = true
+  publicly_accessible     = false
   backup_retention_period = 0
   skip_final_snapshot     = true
   deletion_protection     = false
